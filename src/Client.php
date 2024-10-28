d6c352d61b856bb6d5516aa42c34f3763b632f76
Quentin Delcourt <quentin@delcourt.be>
2024-10-29 00:35:52 +0100

    feat: handle case where the requests limit has been exceeded

--- src/Client.php
+++ src/Client.php
@@ -5,11 +5,19 @@ declare(strict_types=1);
 namespace Kant\StibMivb;
 
 use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7\Response;
+use Kant\StibMivb\Exceptions\RequestLimitExceeded;
+use Psr\Http\Message\ResponseInterface;
+use stdClass;
 
 final class Client
 {
     private const API_ENDPOINT = 'https://stibmivb.opendatasoft.com/api/explore/v2.1/';
+    
     private const PATH_TRAVELLERS_INFORMATION = 'catalog/datasets/travellers-information-rt-production/records';
+    
+    private const HTTP_STATUS_TOO_MANY_REQUESTS = 429;
 
     private GuzzleClient $httpClient;
 
@@ -25,15 +33,29 @@ final class Client
         return new self();
     }
 
+    private function request(string $path): stdClass
+    {
+        try {
+            $response = $this->httpClient->get($path);
+            $content = $response->getBody()->getContents();
+        } catch (ClientException $e) {
+            if ($e->getCode() === self::HTTP_STATUS_TOO_MANY_REQUESTS) {
+                throw RequestLimitExceeded::create();
+            }
+        }
+        
+        
+        return json_decode($content, false, 10, JSON_THROW_ON_ERROR);
+    }
+
     /**
      * @return TravellersInformation[]
      */
     public function latestTravellersInformation(): array
     {
-        $response = $this->httpClient->get(self::PATH_TRAVELLERS_INFORMATION);
-        $content = $response->getBody()->getContents();
-        $deserialized = json_decode($content, false, 10, JSON_THROW_ON_ERROR);
-        
-        return array_map(fn ($travellersInformation) => TravellersInformation::fromObject($travellersInformation) , $deserialized->results);
+        return array_map(
+            fn ($travellersInformation) => TravellersInformation::fromObject($travellersInformation), 
+            $this->request(self::PATH_TRAVELLERS_INFORMATION)->results
+        );
     }
 }
---
+++ src/Exceptions/RequestLimitExceeded.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kant\StibMivb\Exceptions;
+
+use Exception;
+
+final class RequestLimitExceeded extends Exception
+{
+    private function __construct() {}
+
+    public static function create(): self
+    {
+        return new self('The application exceeded the limit of allowed requests.');
+    }
+}
